37f6b586a5e72360faa97a6c876877d4f3df4c7e
Adit0507 <adi05072002@gmail.com>
2024-08-02 12:19:45 +0530

    kinda workss...

--- story.go
+++ story.go
@@ -83,41 +83,50 @@ type HandlerOption func(h *handler)
 
 func WithTemplate(t *template.Template) HandlerOption {
 	return func(h *handler) {
-		h.t = t	
+		h.t = t
 	}
 }
 
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+// NewHandler function to handle requests
 func NewHandler(s Story, opts ...HandlerOption) http.Handler {
-	h := handler{s, tpl}
+	h := handler{s, tpl, defaultPathFn}
 	for _, opt := range opts {
 		opt(&h)
 	}
-	
 	return h
 }
 
 type handler struct {
-	s Story
-	t *template.Template
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
 }
 
-func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func defaultPathFn(r *http.Request) string {
 	path := strings.TrimSpace(r.URL.Path)
 	if path == "" || path == "/" {
 		path = "/intro"
 	}
-	path = path[1:]
 
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
 	if chapter, ok := h.s[path]; ok {
 		err := h.t.Execute(w, chapter)
 		if err != nil {
 			log.Printf("%v", err)
-			http.Error(w, "something went wrong :(", http.StatusInternalServerError)
-
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
 		}
 		return
 	}
-
 	http.Error(w, "Chapter not found.", http.StatusNotFound)
 }
 
@@ -127,17 +136,18 @@ func JSONStory(r io.Reader) (Story, error) {
 	if err := d.Decode(&story); err != nil {
 		return nil, err
 	}
-
 	return story, nil
 }
 
 type Story map[string]Chapter
+
 type Chapter struct {
 	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
 	Options    []Option `json:"options"`
 }
 
+// Option type from json
 type Option struct {
 	Text    string `json:"text"`
 	Chapter string `json:"arc"`
